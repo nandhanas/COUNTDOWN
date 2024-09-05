fd3d1c92ddb703c5d0a811f7ea116d5433482ac9
Nandhana Sakthivel <nsakthi1@login01.g100.cineca.it>
2024-09-05 14:12:02 +0200

    Changes in Nominal frequency

--- src/tool.c
+++ src/tool.c
@@ -632,47 +632,46 @@ HIDDEN int read_intel_nom_freq()
     FILE *fd;
     char *result;
     float nom_freq;
-    printf("im inside nom freq\n");
-    fd = fopen("/proc/cpuinfo", "r");
+   
+    fd = fopen("/sys/devices/system/cpu/cpu0/cpufreq/base_frequency", "r");
     if(fd != NULL)
     {
-    	size_t n = 0;
-   	char *line = NULL;
-    	while(getline(&line, &n, fd) > 0)
-    	{
-        	if(!strncmp(line, "model name", 10))
-        	{
-            		result = strtok(line, ":");
-            		result = strtok(NULL, "@");
-            		result = strtok(NULL, "@");
-            		sscanf(result, " %fGHz", &nom_freq);
-           		break;
-        	}
-    	}
-    	free(line);
-    	fclose(fd);
-        nom_freq *= 1000;
-    }
-    else
-    {
-        fprintf(stderr, "Error: <COUNTDOWN-node:%s-rank:%d> Failed to read file: /proc/cpuinfo  Reading /sys/devices/system/cpu/cpu0/cpufreq/base_frequency file \n",
-            cntd->node.hostname, cntd->rank->world_rank);
-
-        fd = fopen("/sys/devices/system/cpu/cpu0/cpufreq/base_frequency", "r");
-        if(fd != NULL)
-        {
             fscanf(fd, "%f", &nom_freq);
             nom_freq /= pow(10,3);
 
-        }
-        else
-        {
-            fprintf(stderr, "Error: <COUNTDOWN-node:%s-rank:%d> Failed to read file: /sys/devices/system/cpu/cpu0/cpufreq/base_frequency\n",
-               cntd->node.hostname, cntd->rank->world_rank);
-            PMPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-        }
     }
+    else
+    {
+            fprintf(stderr, "Error: <COUNTDOWN-node:%s-rank:%d> Failed to read file: /sys/devices/system/cpu/cpu0/cpufreq/base_frequency\n");
+    
+            fd = fopen("/proc/cpuinfo", "r");
+            if(fd != NULL)
+            {
+                size_t n = 0;
+                char *line = NULL;
+                while(getline(&line, &n, fd) > 0)
+                {
+                        if(!strncmp(line, "model name", 10))
+                        {
+                            result = strtok(line, ":");
+                            result = strtok(NULL, "@");
+                            result = strtok(NULL, "@");
+                            sscanf(result, " %fGHz", &nom_freq);
+                            break;
+                        }
+                }
+                free(line);
+                fclose(fd);
+                nom_freq *= 1000;
+            }
+            else
+            {   
+                fprintf(stderr, "Error: <COUNTDOWN-node:%s-rank:%d> Failed to read file: /proc/cpuinfo . Reading base frequency file\n");
+                PMPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
 
+            }
+        
+    }       
     return (int) (nom_freq);
 }
 #endif
